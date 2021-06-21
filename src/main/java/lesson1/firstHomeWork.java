adb919489a4218093cce7232171162679737eb87
Oleg <chernetskiyoleg09@gmail.com>
2021-06-22 02:00:36 +0300

    Created a method, called "first" and used it in the main method.

--- src/main/java/lesson1/firstHomeWork.java
+++ src/main/java/lesson1/firstHomeWork.java
@@ -2,6 +2,14 @@ package lesson1;
 
 public class firstHomeWork {
     public static void main(String[]args) {
-        
+        first(12,10,24,18);
     }
+
+    /* Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+        где a, b, c, d – аргументы этого метода, имеющие тип float. */
+
+        private static float first(float a, float b, float c, float d) {
+            System.out.println("Результат выражения: " + a * (b + (c / d)));
+            return a * (b + (c / d));
+        }
 }
