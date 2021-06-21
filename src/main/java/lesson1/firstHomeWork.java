28505ddc788e4eeb140776168bb783495baa47d2
Oleg <chernetskiyoleg09@gmail.com>
2021-06-22 02:16:38 +0300

    Created a method, called "fifth" and used it in the main method. + Made all the methods without a return value, because I don't understand why it is needed.

--- src/main/java/lesson1/firstHomeWork.java
+++ src/main/java/lesson1/firstHomeWork.java
@@ -6,52 +6,56 @@ public class firstHomeWork {
         second(12,8);
         third(-3);
         fourth(0);
+        fifth("Игорь");
     }
 
     /* Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
         где a, b, c, d – аргументы этого метода, имеющие тип float. */
 
-        private static float first(float a, float b, float c, float d) {
+        private static void first(float a, float b, float c, float d) {
             System.out.println("Результат выражения: " + a * (b + (c / d)));
-            return a * (b + (c / d));
         }
 
     /* Написать метод, принимающий на вход два целых числа и проверяющий,
         что их сумма лежит в пределах от 10 до 20 (включительно),
         если да – вернуть true, в противном случае – false. */
 
-        private static int second(int a, int b) {
+        private static void second(int a, int b) {
             if (a + b >= 10 && a + b <= 20) {
                 System.out.println(10==10);
             } else {
                 System.out.println(10!=10);
             }
-            return a + b;
         }
 
     /* Написать метод, которому в качестве параметра передается целое число,
         метод должен напечатать в консоль, положительное ли число передали или отрицательное.
         Замечание: ноль считаем положительным числом. */
 
-        private static int third(int a) {
+        private static void third(int a) {
             if (a >= 0) {
                 System.out.println("Введено положительное число");
             } else {
                 System.out.println("Введено отрицательное число");
             }
-            return a;
         }
 
     /* Написать метод, которому в качестве параметра передается целое число.
         Метод должен вернуть true, если число отрицательное,
         и вернуть false если положительное. */
 
-        private static int fourth(int a) {
+        private static void fourth(int a) {
             if (a < 0) {
                 System.out.println(10==10);
             } else {
                 System.out.println(10!=10);
             }
-            return a;
+        }
+
+    /* Написать метод, которому в качестве параметра передается строка, обозначающая имя.
+        Метод должен вывести в консоль сообщение «Привет, указанное_имя!». */
+
+        private static void fifth(String a) {
+            System.out.println("Привет, " + a + "!");
         }
 }
