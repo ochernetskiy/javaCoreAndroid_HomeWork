ea2a31f747d1b58fb81cd9a7edbcf7c3cce0e2fd
Oleg <chernetskiyoleg09@gmail.com>
2021-06-22 02:04:31 +0300

    Created a method, called "second" and used it in the main method.

--- src/main/java/lesson1/firstHomeWork.java
+++ src/main/java/lesson1/firstHomeWork.java
@@ -3,6 +3,7 @@ package lesson1;
 public class firstHomeWork {
     public static void main(String[]args) {
         first(12,10,24,18);
+        second(12,8);
     }
 
     /* Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
@@ -11,5 +12,18 @@ public class firstHomeWork {
         private static float first(float a, float b, float c, float d) {
             System.out.println("Результат выражения: " + a * (b + (c / d)));
             return a * (b + (c / d));
+        }
+
+    /* Написать метод, принимающий на вход два целых числа и проверяющий,
+        что их сумма лежит в пределах от 10 до 20 (включительно),
+        если да – вернуть true, в противном случае – false. */
+
+        private static int second(int a, int b) {
+            if (a + b >= 10 && a + b <= 20) {
+                System.out.println(10==10);
+            } else {
+                System.out.println(10!=10);
+            }
+            return a + b;
         }
 }
