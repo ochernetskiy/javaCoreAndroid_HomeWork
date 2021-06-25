6b4912f161e15a03dd234581e86b6406419ae437
Oleg <chernetskiyoleg09@gmail.com>
2021-06-25 16:23:06 +0300

    Created a new method, called "checkMinMaxArray" and used it in the main method.

--- src/main/java/lesson2/HomeWork2.java
+++ src/main/java/lesson2/HomeWork2.java
@@ -8,10 +8,11 @@ public class HomeWork2 {
         emptyArray();
         anotherArray();
         squareArray();
+        checkMinMaxArray();
     }
 
-    //        Задать целочисленный массив, состоящий из элементов 0 и 1.
-    //        Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    //    #1 Задать целочисленный массив, состоящий из элементов 0 и 1.
+    //    Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
 
     private static void integerArray() {
         int[] integerArr = {0, 1, 1, 0, 1, 0, 0, 1, 0};
@@ -23,7 +24,7 @@ public class HomeWork2 {
         System.out.println(Arrays.toString(integerArr));
     }
 
-    //    Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+    //    #2 Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
 
     private static void emptyArray() {
         int[] emptyArr = new int[8];
@@ -36,7 +37,7 @@ public class HomeWork2 {
         System.out.println(Arrays.toString(emptyArr));
     }
 
-    //    Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+    //    #3 Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
 
     private static void anotherArray() {
         int[] anotherArr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
@@ -47,7 +48,7 @@ public class HomeWork2 {
         System.out.println(Arrays.toString(anotherArr));
     }
 
-    //    Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+    //    #4 Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
     //    и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
 
     private static void squareArray() {
@@ -61,5 +62,25 @@ public class HomeWork2 {
         }
     }
 
+    //    #5 Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+
+    private static void checkMinMaxArray() {
+        int[] checkArr = {1, 356, -293, 568, 12, -756, 856};
+        int min = 0;
+        int max = 0;
+
+        for (int i = 0; i < checkArr.length; i++) {
+            if (checkArr[i] < min) {
+                min = checkArr[i];
+            }
+            if (checkArr[i] > max) {
+                max = checkArr[i];
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(min);
+
+    }
 
 }
