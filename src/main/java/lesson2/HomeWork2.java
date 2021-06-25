97dd9c5d74ffb26ce2f8de25bd20b8425f34da52
Oleg <chernetskiyoleg09@gmail.com>
2021-06-25 14:25:09 +0300

    Created new method called "emptyArray" and used it in the main method.

--- src/main/java/lesson2/HomeWork2.java
+++ src/main/java/lesson2/HomeWork2.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class HomeWork2 {
     public static void main(String[] args) {
         integerArray();
+        emptyArray();
     }
 
     //        Задать целочисленный массив, состоящий из элементов 0 и 1.
@@ -18,6 +19,19 @@ public class HomeWork2 {
         }
 
         System.out.println(Arrays.toString(integerArr));
+    }
+
+    //    Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+
+    private static void emptyArray() {
+        int[] emptyArr = new int[8];
+        int j = 0;
+        for (int i = 0; i < emptyArr.length; i++) {
+            emptyArr[i] = j;
+            j += 3;
+        }
+
+        System.out.println(Arrays.toString(emptyArr));
     }
 
 }
